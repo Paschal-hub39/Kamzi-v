7e9d194555eea9528960f494da580b6886cc5569
Paschal-hub39 <azubuikepaschal6@gmail.com>
2026-04-26 02:40:06 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['slwmkfdkoxfsouiffkut.supabase.co'],
+  },
+}
+
+module.exports = nextConfig
